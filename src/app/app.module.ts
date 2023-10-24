7bad096d157f22e00badb772a9d12bce37d34e11
Henrique Fernandez <rickfernandez03@gmail.com>
2023-10-24 18:02:42 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule, NoopAnimationsModule } from  '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { ListaTarefasComponent } from './lista-tarefas/lista-tarefas.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { MensagemComponent } from './componentes/mensagem/mensagem.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaTarefasComponent,
+    CabecalhoComponent,
+    MensagemComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    // NoopAnimationsModule ---> Desabilita toda as animações
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
